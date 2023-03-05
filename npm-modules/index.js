951b6b08f2685b6824a95960c4ee807ac2352c0d
jswan <jswannieman@gmail.com>
2023-03-06 09:46:29 +1100

    Update index Javascript Component

--- npm-modules/index.js
+++ npm-modules/index.js
@@ -3,16 +3,24 @@ const logEvents = require("./logEvents");
 const EventEmitter = require("events");
 const { setTimeout } = require("timers");
 
-class MyEmitter extends EventEmitter {}
+class Emitter extends EventEmitter {}
 
 // Initialize object
 
-const myEmitter = new MyEmitter();
+const myEmitter = new Emitter();
 
 // Add listener for the log event
 
-myEmitter.on("log", (msg) => logEvents(msg));
+myEmitter.on("logger", (msg) => logEvents(msg));
+
+let x = 0;
+
+const logInterval = setInterval(logTimer, 2000);
+
+function logTimer() {
+  myEmitter.emit("logger", "Log event emitted!");
+}
 
 setTimeout(() => {
-  myEmitter.emit("log", "Log event emitted!");
-}, 5000);
+  clearInterval(logInterval);
+}, 16000);
